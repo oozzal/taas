c58953ae67efac862688e614e026c219be618987
oozzal <theoozzal@gmail.com>
2018-11-08 14:44:53 +0545

    Init

---
+++ lib/taas.ex
@@ -0,0 +1,18 @@
+defmodule Taas do
+  @moduledoc """
+  Documentation for Taas.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taas_test.exs
@@ -0,0 +1,8 @@
+defmodule TaasTest do
+  use ExUnit.Case
+  doctest Taas
+
+  test "greets the world" do
+    assert Taas.hello() == :world
+  end
+end

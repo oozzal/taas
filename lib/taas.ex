b645909acaac8b2977af1349c803beafe0510c5a
oozzal <theoozzal@gmail.com>
2019-07-13 14:33:18 +0545

    Add deal and show methods to deck

--- lib/taas.ex
+++ lib/taas.ex
@@ -1,7 +1,6 @@
 defmodule Taas do
   def run(total_players, cards_per_hand) do
-    Deck.get_instance
-    |> Deck.get_hands([], cards_per_hand, total_players)
-    |> Enum.map(fn h -> Deck.nice_print(h) end)
+    Deck.deal(total_players, cards_per_hand)
+    |> Deck.show()
   end
 end
